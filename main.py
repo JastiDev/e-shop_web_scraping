5b4a513ec380f538e05ce9a16846dcc2e2ecc2b7
aitrainer <59392981+JastiDev@users.noreply.github.com>
2025-02-17 12:32:45 -0600

    feat: :heart: Add new features to check the pagination
    I updated the feature to get more content by passing the pagination. :+1:

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
@@ -7,35 +8,39 @@ import time
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1024,768")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+# options.add_argument("--disable-gpu")
+# options.add_argument("--window-size=1024,768")
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(service=Service
                           (ChromeDriverManager().install()),
                           options=options)
 
-url = "https://www.ebay.com/b/Type-Chopper/6024/bn_57336502"
-driver.get(url)
+base_url = "https://www.ebay.com/b/PC-Laptops-Netbooks/177/bn_317584?_pgn="
 
-time.sleep(3)
+num_pages = 5
 
-product_list = []
-products = driver.find_elements(By.CLASS_NAME, "brwrvr__item-card")
+for page in range(1, num_pages + 1):
+    url = f"{base_url}{page}"
+    driver.get(url)
+    time.sleep(10)
 
-for product in products:
-    try:
-        title = product.find_element(By.CLASS_NAME, "bsig__title__text").text
-    except:
-        title = "Title not found"
+    product_list = []
+    products = driver.find_elements(By.CLASS_NAME, "brwrvr__item-card")
 
-    try: 
-        price = product.find_element(By.CLASS_NAME, "bsig__price").text
-    except:
-        price = "Price not found"
+    for product in products:
+        try:
+            title = product.find_element(By.CLASS_NAME, "bsig__title__text").text
+        except:
+            title = "Title not found"
 
-    product_list.append([title, price])
+        try: 
+            price = product.find_element(By.CLASS_NAME, "bsig__price").text
+        except:
+            price = "Price not found"
+
+        product_list.append([title, price])
 
 driver.quit()
 
